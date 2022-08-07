c7706112bc63f86d96ffa7acfc2f5cd7408401a5
TraianC299 <traianciochina@gmail.com>
2022-08-07 11:41:37 +0300

    Initial Commit

---
+++ RestaurantMenuApp/RestaurantMenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantMenuAppApp.swift
+//  RestaurantMenuApp
+//
+//  Created by Traian on 07.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantMenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
